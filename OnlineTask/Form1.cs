50b6d412514352f1e14e13dd08cb4298fe14c0be
My Name <skofenko02@mail.ru>
2022-05-13 09:32:11 +0300

    channels added

--- OnlineTask/Form1.cs
+++ OnlineTask/Form1.cs
@@ -1,22 +1,25 @@
-﻿using NetManager;
-using FoundationLibrary.TransformSignal;
+﻿using FoundationLibrary.TransformSignal;
+using NetManager;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OnlineTask
 {
     public partial class Form1 : Form
     {
+        List<double>[] channels = new List<double>[4];
+        int[] channelsInd = { 2, 6, 13, 17 };
+        int window = 24 * 20;
         public Form1()
         {
             InitializeComponent();
+            for(int i = 0; i< channels.Length; i++)
+            {
+                channels[i] = new List<double>(window);
+            }
             reseiveClientControl.Client.Reseive += Client_Reseive;
             reseiveClientControl.Client.Error += Client_Error;
         }
@@ -29,10 +32,22 @@ namespace OnlineTask
         private void Client_Reseive(object sender, EventClientMsgArgs e)
         {
             Frame frame = new Frame(e.Msg);
-            double[] data = frame.Data.Select(x => Convert.ToDouble(x)).ToArray();
-            alglib.complex[] fur = new alglib.complex[frame.Data.Length];
-            Trend.SubtractionTrend(data);
-            alglib.fftr1d(data, out fur);
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 24; j++)
+                {
+                    channels[i].Add(Convert.ToDouble(frame.Data[channelsInd[i] * 24 + j]));
+                }
+            }
+            if (channels[0].Count == window)
+            {               
+                for (int i = 0; i < 4; i++)
+                {
+                    double[] data = channels[i].ToArray();
+                    Trend.SubtractionTrend(data);
+                    alglib.fftr1d(data, out alglib.complex[] fur);
+                }
+            }
         }
 
         private void Button_Click(object sender, EventArgs e)
