027e601b2546849f49d9b1d6927f10ae1e2550ad
My Name <skofenko02@mail.ru>
2022-05-13 03:06:05 +0300

    Add project files.

---
+++ OnlineTask/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace OnlineTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.reseiveClientControl = new NetManager.Client.ReseiveClientControl();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reseiveClientControl
+            // 
+            this.reseiveClientControl.IPServer = ((System.Net.IPAddress)(resources.GetObject("reseiveClientControl.IPServer")));
+            this.reseiveClientControl.IsSyncronized = true;
+            this.reseiveClientControl.Location = new System.Drawing.Point(12, 12);
+            this.reseiveClientControl.Name = "reseiveClientControl";
+            this.reseiveClientControl.Size = new System.Drawing.Size(174, 98);
+            this.reseiveClientControl.TabIndex = 4;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(55, 116);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 5;
+            this.button.Text = "Старт";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.reseiveClientControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NetManager.Client.ReseiveClientControl reseiveClientControl;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ OnlineTask/Form1.cs
@@ -0,0 +1,52 @@
+﻿using NetManager;
+using FoundationLibrary.TransformSignal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            reseiveClientControl.Client.Reseive += Client_Reseive;
+            reseiveClientControl.Client.Error += Client_Error;
+        }
+
+        private void Client_Error(object sender, EventMsgArgs e)
+        {
+            MessageBox.Show(e.Msg, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void Client_Reseive(object sender, EventClientMsgArgs e)
+        {
+            Frame frame = new Frame(e.Msg);
+            double[] data = frame.Data.Select(x => Convert.ToDouble(x)).ToArray();
+            alglib.complex[] fur = new alglib.complex[frame.Data.Length];
+            Trend.SubtractionTrend(data);
+            alglib.fftr1d(data, out fur);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (!reseiveClientControl.Client.IsRunning)
+            {
+                reseiveClientControl.Client.StartClient();
+                button.Text = "Стоп";
+            }
+            else
+            {
+                reseiveClientControl.Client.StopClient();
+                button.Text = "Старт";
+            }
+        }
+    }
+}
