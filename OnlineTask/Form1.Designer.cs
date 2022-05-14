f3c5d110937c8a928fa2593e10c35fb89eec2146
My Name <skofenko02@mail.ru>
2022-05-14 09:33:43 +0300

    charts

--- OnlineTask/Form1.Designer.cs
+++ OnlineTask/Form1.Designer.cs
@@ -28,23 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.reseiveClientControl = new NetManager.Client.ReseiveClientControl();
             this.button = new System.Windows.Forms.Button();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // reseiveClientControl
             // 
+            this.reseiveClientControl.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.reseiveClientControl.IPServer = ((System.Net.IPAddress)(resources.GetObject("reseiveClientControl.IPServer")));
             this.reseiveClientControl.IsSyncronized = true;
-            this.reseiveClientControl.Location = new System.Drawing.Point(12, 12);
+            this.reseiveClientControl.Location = new System.Drawing.Point(544, 12);
             this.reseiveClientControl.Name = "reseiveClientControl";
             this.reseiveClientControl.Size = new System.Drawing.Size(174, 98);
             this.reseiveClientControl.TabIndex = 4;
             // 
             // button
             // 
-            this.button.Location = new System.Drawing.Point(55, 116);
+            this.button.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button.Location = new System.Drawing.Point(598, 116);
             this.button.Name = "button";
             this.button.Size = new System.Drawing.Size(75, 23);
             this.button.TabIndex = 5;
@@ -52,15 +76,89 @@
             this.button.UseVisualStyleBackColor = true;
             this.button.Click += new System.EventHandler(this.Button_Click);
             // 
+            // chart3
+            // 
+            this.chart3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart3.Legends.Add(legend1);
+            this.chart3.Location = new System.Drawing.Point(752, 13);
+            this.chart3.Name = "chart3";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart3.Series.Add(series1);
+            this.chart3.Size = new System.Drawing.Size(500, 250);
+            this.chart3.TabIndex = 6;
+            this.chart3.Text = "chart1";
+            // 
+            // chart4
+            // 
+            this.chart4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart4.Legends.Add(legend2);
+            this.chart4.Location = new System.Drawing.Point(752, 269);
+            this.chart4.Name = "chart4";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart4.Series.Add(series2);
+            this.chart4.Size = new System.Drawing.Size(500, 250);
+            this.chart4.TabIndex = 7;
+            this.chart4.Text = "chart2";
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(13, 13);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(500, 250);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart3";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(12, 269);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(500, 250);
+            this.chart2.TabIndex = 9;
+            this.chart2.Text = "chart4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
             this.Controls.Add(this.button);
             this.Controls.Add(this.reseiveClientControl);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +167,11 @@
 
         private NetManager.Client.ReseiveClientControl reseiveClientControl;
         private System.Windows.Forms.Button button;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- OnlineTask/Form1.cs
+++ OnlineTask/Form1.cs
@@ -2,26 +2,65 @@
 using NetManager;
 using System;
 using System.Collections.Generic;
-using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace OnlineTask
 {
     public partial class Form1 : Form
     {
-        List<double>[] channels = new List<double>[4];
+        Chart[] charts;
+        LinkedList<double>[] channels = new LinkedList<double>[4];
         int[] channelsInd = { 2, 6, 13, 17 };
-        int window = 24 * 20;
+        int sampleRate = 1000;
+        int window;
+        int direction = 0;
+
         public Form1()
         {
             InitializeComponent();
-            for(int i = 0; i< channels.Length; i++)
+            window = (int)(24 * Math.Ceiling(sampleRate / 24.0));
+            for (int i = 0; i < channels.Length; i++)
             {
-                channels[i] = new List<double>(window);
+                channels[i] = new LinkedList<double>();
+            }
+            charts = new Chart[] { chart1, chart2, chart3, chart4 };
+            String[] titles = { "C3", "T3", "C4", "T4" };
+            for (int i = 0; i < charts.Length; i++)
+            {
+                var title = new Title(titles[i])
+                {
+                    Font = new Font("Arial", 12, FontStyle.Bold)
+                };
+                charts[i].Titles.Add(title);
+                charts[i].Legends.Clear();
+
+                var graph = charts[i].Series[0];
+                graph.ChartType = SeriesChartType.FastLine;
+                graph.Points.AddXY(0, 0);
+
+                var area = charts[i].ChartAreas[0];
+                area.AxisX.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+                area.AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+                area.AxisX.ScaleView.Zoomable = true;
+                area.AxisX.Maximum = window / 24;
+                area.CursorX.AutoScroll = true;
+                area.CursorX.IsUserSelectionEnabled = true;
             }
             reseiveClientControl.Client.Reseive += Client_Reseive;
             reseiveClientControl.Client.Error += Client_Error;
+            timer.Interval = 1;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Cursor.Position = new Point(
+                System.Windows.Forms.Cursor.Position.X + 2 * direction,
+                System.Windows.Forms.Cursor.Position.Y);
         }
 
         private void Client_Error(object sender, EventMsgArgs e)
@@ -36,17 +75,27 @@ namespace OnlineTask
             {
                 for (int j = 0; j < 24; j++)
                 {
-                    channels[i].Add(Convert.ToDouble(frame.Data[channelsInd[i] * 24 + j]));
+                    double y = Convert.ToDouble(frame.Data[channelsInd[i] * 24 + j]);
+                    channels[i].AddLast(y);
+                    Invoke(new Action(() => charts[i].Series[0].Points.AddY(y)));
                 }
+                while (channels[i].Count > window)
+                {
+                    channels[i].RemoveFirst();
+                }
+                if (charts[i].Series[0].Points.Count == window / 24)
+                {
+                    Invoke(new Action(() => charts[i].Series[0].Points.Clear()));
+                }
+                
             }
             if (channels[0].Count == window)
-            {               
+            {
                 for (int i = 0; i < 4; i++)
                 {
                     double[] data = channels[i].ToArray();
                     Trend.SubtractionTrend(data);
                     alglib.fftr1d(data, out alglib.complex[] fur);
-                    channels[i].Clear();
                 }
             }
         }
